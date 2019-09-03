c3ecc7e4a5900c1c5ca1ac8abf497f71efecaba1
Thiago-F <thiago_fialho150@hotmail.com>
2019-09-03 19:09:08 -0300

    ciclo de vida de um componente

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -3,18 +3,36 @@ import React, { Component } from 'react';
 import TechItem from './TechItem'
 
 class TechList extends Component {
+  static propTypes = {
+    // tech: PropTypes.string,
+  }
+
   static defaultProps = {
     // tech : 'Oculto'
   }
 
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJs',
-      'React Native'
-    ]
+    techs: []
   }
+
+  //é chamado antes do componente aparecer
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) })
+    }
+  }
+  //exec quando houver alterações
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      //toda vez que o componente muda atualiza aqui
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+  //executado quando o componente deixa de existir
+  componentWillUnmount() { }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -46,7 +64,6 @@ class TechList extends Component {
               />
             )
           }
-          <TechItem />
         </ul>
         <input
           type="text"
