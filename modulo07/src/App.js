9de17d1771bc63c57967a7b7421bd60917fd4b48
Thiago-F <thiago_fialho150@hotmail.com>
2019-09-11 14:57:10 -0300

    config reactotron

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
+import './config/ReactotonConfig';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
