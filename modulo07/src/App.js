334e1f55d998f57e420577c27f930c31855847c3
Thiago-F <thiago_fialho150@hotmail.com>
2019-09-11 11:53:37 -0300

    estilizando home

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
@@ -7,7 +9,7 @@ import GlobalStyle from './styles/global';
 function App() {
     return (
         <BrowserRouter>
-            {/* <Header /> */}
+            <Header />
             <Routes />
             <GlobalStyle />
         </BrowserRouter>
