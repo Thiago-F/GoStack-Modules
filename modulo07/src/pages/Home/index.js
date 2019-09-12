46417eb2ec990bc8d77e6a57b2774d5e392cba68
Thiago-F <thiago_fialho150@hotmail.com>
2019-09-12 10:48:09 -0300

    exibindo quantidades

--- modulo07/src/pages/Home/index.js
+++ modulo07/src/pages/Home/index.js
@@ -34,6 +34,8 @@ class Home extends Component {
 
     render() {
         const { products } = this.state;
+        const { amount } = this.props;
+
         return (
             <ProductList>
                 {products.map(product => (
@@ -47,7 +49,8 @@ class Home extends Component {
                             onClick={() => this.handleAddProduct(product)}
                         >
                             <div>
-                                <MdAddShoppingCart size={16} color="#fff" />
+                                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                                {amount[product.id] || 0}
                             </div>
                             <span>ADICIONAR AO CARRINHO</span>
                         </button>
@@ -61,7 +64,15 @@ class Home extends Component {
 const mapDispatchToProps = dispatch =>
     bindActionCreators(CartActions, dispatch);
 
+const mapStateToProps = state => ({
+    amount: state.cart.reduce((amount, product) => {
+        amount[product.id] = product.amount;
+
+        return amount;
+    }, {}),
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(Home);
