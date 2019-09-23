7e8e60118f517a2282f8890872c15011d03abd36
Thiago-F <thiago_fialho150@hotmail.com>
2019-09-23 20:36:58 -0300

    initial commit

---
+++ modulo06/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Thiago Fialho</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
