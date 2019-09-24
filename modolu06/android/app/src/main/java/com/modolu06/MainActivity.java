2daa31e5d932d5861556efb9a2179c54da065994
Thiago-F <thiago_fialho150@hotmail.com>
2019-09-24 19:23:07 -0300

    init proj

---
+++ modolu06/android/app/src/main/java/com/modolu06/MainActivity.java
@@ -0,0 +1,15 @@
+package com.modolu06;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "modolu06";
+  }
+}
